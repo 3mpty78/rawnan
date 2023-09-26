5869249e1390e4876b0b3d63fd21553b2a15d00c
Aurélien <105590579+3mpty78@users.noreply.github.com>
2023-09-26 21:13:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,21 +32,21 @@ document.addEventListener("touchmove", (e) => {
 
 // Toggler de la navigation
 
-const btn = document.querySelector(".nav-toggler");
+const button = document.querySelector(".nav-toggler");
 const header = document.querySelector("header");
 
 document.addEventListener("click", (e) => {
       if (e.target.closest(".nav-toggler")) {
-            btn.classList.toggle("toggle");
+            button.classList.toggle("toggle");
             header.classList.toggle("active");
       } else if (!e.target.closest(".nav-toggler")) {
-            btn.classList.remove("toggle");
+            button.classList.remove("toggle");
             header.classList.remove("active");
       }
 });
 document.addEventListener("scroll", (f) => {
       if (window.scrollY > 50) {
-            btn.classList.remove("toggle");
+            button.classList.remove("toggle");
             header.classList.remove("active");
       }
 });
